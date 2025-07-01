08aaa8b1061c087e922b489f9c07eed777f57a8c
Jitesh-Bhatti <hiteshilpa0@gmail.com>
2025-07-02 00:02:49 +0530

    Day 2 Solved #3 and #3330

---
+++ 3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,33 @@
+'''
+3. Longest Substring Without Repeating Characters
+
+Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+'''
+
+# We cant do it with string because operation on set is faster then slicing and we have to slice or remove element of s from our string which is quit longer and harder for time and space complexcity.
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_set = set()
+        left = 0
+        max_len = 0
+
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+            char_set.add(s[right])
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
---
+++ 3330. Find the Original Typed String.py
@@ -0,0 +1,55 @@
+'''
+
+3330. Find the Original Typed String I
+
+Alice is attempting to type a specific string on her computer. However, she tends to be clumsy and may press a key for too long, resulting in a character being typed multiple times.
+
+Although Alice tried to focus on her typing, she is aware that she may still have done this at most once.
+
+You are given a string word, which represents the final output displayed on Alice's screen.
+
+Return the total number of possible original strings that Alice might have intended to type.
+
+ 
+
+Example 1:
+
+Input: word = "abbcccc"
+
+Output: 5
+
+Explanation:
+
+The possible strings are: "abbcccc", "abbccc", "abbcc", "abbc", and "abcccc".
+'''
+
+class Solution(object):
+    def possibleStringCount(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        rep = {}
+        letter = set()
+        possibility = 0
+        for i in word:
+            if i not in rep:
+                rep[i] = word.count(i)
+                possibility += 1
+        print(rep.values())
+        a = 0
+        for i in rep.values():
+            if(i == 1):
+                a = a+i
+            else:
+                a = a+i
+                a -= 1
+        if(a == possibility){
+            return 1
+        }
+        else {
+            return a
+        }
+
+A = Solution()
+A.possibleStringCount("abcd")
